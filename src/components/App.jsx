84d944093c23da553035b756e80ec59d5df4b8e0
Palmi Moller <palmimoller@Palmis-MacBook-Pro.local>
2020-01-15 12:08:22 -0700

    filter by threat level

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,8 +6,8 @@ import exampleData from '../example-data/exampleData';
 import '../styles/App.scss';
 
 class App extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       filter: 'None',
       bugs: exampleData,
@@ -15,8 +15,10 @@ class App extends React.Component {
     this.filterHandler = this.filterHandler.bind(this);
   }
 
-  filterHandler(filter) {
-    this.setState({ filter });
+  filterHandler(priority) {
+    console.log('clicked', priority)
+    this.setState({ filter: priority });
+    console.log(this.state)
   }
 
   render() {
@@ -25,20 +27,37 @@ class App extends React.Component {
         <Nav
           filterHandler={this.filterHandler}
         />
-        {this.state.bugs.map((bug) => (
-          <BugTile
-            bugName={bug.bugName}
-            bugDescription={bug.bugDescription}
-            reportedBy={bug.reportedBy}
-            createdDate={bug.createdDate}
-            assignedTo={bug.assignedTo}
-            threatLevel={bug.threatLevel}
-            key={bug.bugName}
-          />
-        ))}
+        {this.state.bugs.map((bug) => {
+          // dont map over values that dont match the conditional below
+          if (bug.threatLevel === this.state.filter || this.state.filter === 'None') {
+            return (
+              <BugTile
+                bugName={bug.bugName}
+                bugDescription={bug.bugDescription}
+                reportedBy={bug.reportedBy}
+                createdDate={bug.createdDate}
+                assignedTo={bug.assignedTo}
+                threatLevel={bug.threatLevel}
+                key={bug.bugName}
+              />
+            )
+          }
+        })}
       </table>
     );
   }
 }
 
 export default App;
+
+
+// FRONT-END
+// Filter bugs by the threat level set by the user.
+
+// Create a modal that will allow users to submit new bugs. A bug report must contain:
+
+// Assigned Team Member
+// Threat Level
+// Description
+// Reporter
+// Populate the client with bug reports sent back from the server.
